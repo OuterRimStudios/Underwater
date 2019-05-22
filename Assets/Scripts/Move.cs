29351906c731480bb88308b47857549bd7f1bfda
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-05-22 14:19:53 -0500

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Camera.main.transform.TransformDirection(move);
+        transform.position += move.normalized * speed * Time.deltaTime;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -57, 57), Mathf.Clamp(transform.position.y, 7, 42.5f), Mathf.Clamp(transform.position.z, -57, 57));
+    }
+}
